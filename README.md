86d91e9d9c224c8bc82c01764c16bd7812882270
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-01-07 16:22:22 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Notify by Script Condition
+===================
+
+... extension for Google Chrome browser.
+
+Now available on [Chrome Web Store]()
+
+=========
+
+Google Chrome için ... uzantısı.
+
+Notify by Script Condition artık [Chrome Web Mağazası'nda]()
---
+++ main.js
@@ -0,0 +1,54 @@
+/**
+ * Google Chrome Browser extension | Notify by Script Condition
+ *
+ * @author Ufuk Uzun
+ */
+
+// Create alarm
+// Documentation for "chrome.alarms": https://developer.chrome.com/extensions/alarms
+chrome.alarms.create("OFFENE_STELLEN_ALARM", {periodInMinutes: 1});
+
+// Configure what will happened when alarm triggered
+chrome.alarms.onAlarm.addListener(function (alarm) {
+    console.log("Alarm triggered: " + alarm.name);
+
+    $.get("http://www.innflow.com/offene-stellen", function (responseText) {
+        console.log("OK");
+
+        // Find out public model that using to render page by "wix"
+        var extractedPublicModel = JSON.parse(responseText.match(/var publicModel = {.*}/g)[0].replace('var publicModel = ', ''));
+        extractedPublicModel.pageList.pages.forEach(function (e) {
+            if (e.title == 'OFFENE STELLEN') {
+                console.log(e);
+
+                $.get("https://static.wixstatic.com/sites/" + e.pageJsonFileName + ".z?v=3", function (responseJson) {
+                    // Get item count
+                    var itemCount = $(JSON.stringify(responseJson).match(/<ul.*<\/ul>/g)[0]).find('li').length;
+                    if (itemCount > 6) {
+                        var message = "New item! Current item count: " + itemCount;
+                        console.log(message);
+                        notify("New item!", message);
+                    } else {
+                        var message = "No new item. Current item count: " + itemCount;
+                        console.log(message);
+                        notify("No new item", message);
+                    }
+                }).fail(function () {
+                    console.log("ERROR");
+                });
+            }
+        });
+    }).fail(function () {
+        console.log("ERROR");
+    });
+});
+
+// Documentation for "chrome.notifications": https://developer.chrome.com/extensions/notifications
+function notify(title, message) {
+    chrome.notifications.create("notify-by-condition", {
+        "type": "basic",
+        "title": title,
+        "iconUrl": "https://cdn2.iconfinder.com/data/icons/business-office-6/100/f8-01-512.png",
+        "message": message
+    });
+};
