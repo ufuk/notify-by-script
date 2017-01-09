d0378510142883c840b55fe4dc992bf87fa21270
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-01-10 00:54:56 +0300

    evaluated script implementation. checks usd-try fx rate in every five minutes and displays within a notification.

--- main.js
+++ main.js
@@ -6,41 +6,24 @@
 
 // Create alarm
 // Documentation for "chrome.alarms": https://developer.chrome.com/extensions/alarms
-chrome.alarms.create("OFFENE_STELLEN_ALARM", {periodInMinutes: 1});
+chrome.alarms.create("USD_TO_TRY_FX_RATE", {periodInMinutes: 5});
 
 // Configure what will happened when alarm triggered
 chrome.alarms.onAlarm.addListener(function (alarm) {
     console.log("Alarm triggered: " + alarm.name);
 
-    $.get("http://www.innflow.com/offene-stellen", function (responseText) {
-        console.log("OK");
+    var result = eval(
+        "(function () {" +
+        "    var responseText = $.ajax({" +
+        "        type: 'GET'," +
+        "        url: 'http://kur.doviz.com/serbest-piyasa/amerikan-dolari'," +
+        "        async: false" +
+        "    }).responseText;" +
+        "return $($($.parseHTML(responseText)).find('.flag.flag-USD').parent().parent().find('span.color-green')[0]).text();" +
+        "})()"
+    );
 
-        // Find out public model that using to render page by "wix"
-        var extractedPublicModel = JSON.parse(responseText.match(/var publicModel = {.*}/g)[0].replace('var publicModel = ', ''));
-        extractedPublicModel.pageList.pages.forEach(function (e) {
-            if (e.title == 'OFFENE STELLEN') {
-                console.log(e);
-
-                $.get("https://static.wixstatic.com/sites/" + e.pageJsonFileName + ".z?v=3", function (responseJson) {
-                    // Get item count
-                    var itemCount = $(JSON.stringify(responseJson).match(/<ul.*<\/ul>/g)[0]).find('li').length;
-                    if (itemCount > 6) {
-                        var message = "New item! Current item count: " + itemCount;
-                        console.log(message);
-                        notify("New item!", message);
-                    } else {
-                        var message = "No new item. Current item count: " + itemCount;
-                        console.log(message);
-                        notify("No new item", message);
-                    }
-                }).fail(function () {
-                    console.log("ERROR");
-                });
-            }
-        });
-    }).fail(function () {
-        console.log("ERROR");
-    });
+    notify("USD / TRY", result);
 });
 
 // Documentation for "chrome.notifications": https://developer.chrome.com/extensions/notifications
