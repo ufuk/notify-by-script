e54b13e3b7c0f8350fb6f109cbf47b89c151fa4b
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-01-13 14:33:01 +0300

    bug fix: prevent duplicate notification and notify for latest alarm

--- main.js
+++ main.js
@@ -4,11 +4,12 @@ function createAlarm() {
     getAlarmOptions(function (options) {
         chrome.alarms.clearAll(function () {
             console.log("Alarms cleared.");
-        });
 
-        if (options && options.activated) {
-            chrome.alarms.create("notify-by-script-condition", {periodInMinutes: options.period});
-        }
+            if (options && options.activated) {
+                chrome.alarms.create("notify-by-script-condition", {periodInMinutes: options.period});
+                console.log("Alarms created.");
+            }
+        });
     });
 }
 createAlarm();
@@ -22,26 +23,48 @@ chrome.alarms.onAlarm.addListener(function (alarm) {
             var result = eval(options.script);
 
             if (result != undefined) {
-                notify(result.title, result.message);
+                notify(alarm.name, result.title, result.message);
             }
         }
     });
 });
 
 // Documentation for "chrome.notifications": https://developer.chrome.com/extensions/notifications
-function notify(title, message) {
-    chrome.notifications.create("notify-by-script-condition-" + new Date().getTime(), {
-        "type": "basic",
-        "title": title,
-        "iconUrl": "https://raw.githubusercontent.com/ufuk/notify-by-script-condition/master/icon.png",
-        "message": message
+function notify(alarmName, title, message) {
+    chrome.notifications.getAll(function (notifications) {
+        console.log("Current notification ids: " + JSON.stringify(notifications));
+
+        var notificationIds = Object.keys(notifications);
+        for (var i = 0; i < notificationIds.length; i++) {
+            var eachNotificationId = notificationIds[i];
+            if (eachNotificationId.startsWith(alarmName)) {
+                console.log("Notification already exists: " + eachNotificationId);
+                chrome.notifications.clear(eachNotificationId, function (wasCleared) {
+                    if (wasCleared) {
+                        console.log("Notification cleared: " + eachNotificationId);
+                    } else {
+                        console.log("Notification couldn't be cleared: " + eachNotificationId);
+                    }
+                });
+            }
+        }
+
+        var notificationId = alarmName + "-" + new Date().getTime();
+        chrome.notifications.create(notificationId, {
+            "type": "basic",
+            "title": title,
+            "iconUrl": "https://raw.githubusercontent.com/ufuk/notify-by-script-condition/master/icon.png",
+            "message": message
+        }, function () {
+            console.log("Notification created: " + notificationId);
+        });
     });
 }
 
 // New options listener
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        console.log(sender);
+        console.log("Message received: " + request.message);
 
         if (request.message == "newOptionsSaved") {
             createAlarm();
