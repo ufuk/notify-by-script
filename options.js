a3bfe74cf8d323d87b6aa5d57bb9d96a39e0b194
Ufuk Uzun <ufukuzun.ce@gmail.com>
2017-01-15 20:34:46 +0300

    editor improvements: monospace font & tab support & wrap off

--- options.js
+++ options.js
@@ -91,3 +91,15 @@ function displayStatus(statusText) {
 document.addEventListener('DOMContentLoaded', restoreOptions);
 document.getElementById('save').addEventListener('click', saveOptions);
 document.getElementById('testScript').addEventListener('click', testScript);
+document.getElementById('editor').addEventListener('keydown', onEditorKeyDown);
+
+function onEditorKeyDown(event) {
+    if (event.keyCode === 9) {
+        event.preventDefault();
+        var v = this.value;
+        var s = this.selectionStart;
+        var e = this.selectionEnd;
+        this.value = v.substring(0, s) + '    ' + v.substring(e);
+        this.selectionStart = this.selectionEnd = s + 4;
+    }
+}
